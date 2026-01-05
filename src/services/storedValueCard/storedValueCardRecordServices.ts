0175670ae3360b31abeb78cff2420f7a7ab9dc19
hsieh435 <hsieh435@gmail.com>
2026-01-05 22:52:13 +0800

    20250105

--- src/services/storedValueCard/storedValueCardRecordServices.ts
+++ src/services/storedValueCard/storedValueCardRecordServices.ts
@@ -85,7 +85,6 @@ export async function insertStoredValueCardRecord(data: IStoredValueCardRecordDa
 
 
   // console.log("dateDetectResult:", dateDetectResult);
-  // if (!dateDetectResult.success) return { success: false, message: dateDetectResult.message };
   if (!dateDetectResult.success) {
     return { success: false, message: dateDetectResult.message };
   } else if (dateDetectResult.success) {
@@ -98,16 +97,40 @@ export async function insertStoredValueCardRecord(data: IStoredValueCardRecordDa
 
 
   try {
-    const insertResult = await pool.query(`
+    const insertQuery = `
       INSERT INTO public.stored_value_card_trade(trade_id, stored_value_card_id, user_id, trade_datetime, trade_category, transaction_type, trade_amount, remaining_amount, currency, trade_description, trade_note)
       VALUES ('SVC-${data.updateData.currency}-${getCurrentTimestamp()}', '${data.updateData.storedValueCardId}', '${data.userId}', '${data.updateData.tradeDatetime}', '${data.updateData.tradeCategory}', '${data.updateData.transactionType}', ${data.updateData.tradeAmount}, ${data.updateData.remainingAmount}, '${data.updateData.currency}', '${data.updateData.tradeDescription}', '${data.updateData.tradeNote}')
-    `);
+    `;
+    const params = [
+      data.updateData.tradeId,
+      data.updateData.storedValueCardId,
+      data.userId,
+      data.updateData.tradeDatetime,
+      data.updateData.tradeCategory,
+      data.updateData.transactionType,
+      data.updateData.tradeAmount,
+      data.updateData.remainingAmount,
+      data.updateData.currency,
+      data.updateData.tradeDescription,
+      data.updateData.tradeNote,
+    ];
+    await pool.query(insertQuery, params);
 
-    return insertResult.rowCount === 1
-      ? { success: true, userData: keysToCamel(insertResult.rows[0]) }
-      : { success: false, userData: [] };
-  } catch (err) {
-    return { success: false, message: err instanceof Error ? err.message : String(err) };
+    await updateRelatedData(
+      "stored_value_card_list",
+      "stored_value_card_trade",
+      "stored_value_card_id",
+      data.updateData.storedValueCardId,
+      data.updateData.tradeDatetime,
+      data.updateData.transactionType,
+      data.oriData.oriTransactionType,
+      data.updateData.tradeAmount,
+      data.oriData.oriTradeAmount,
+    );
+    // return { success: true, message: "新增成功" };
+    return { success: false, message: "新增失敗" };
+  } catch (error) {
+    return { success: false, message: "新增失敗" };
   }
 }
 
